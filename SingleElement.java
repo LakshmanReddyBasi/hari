5f396b7c240a0f4fbc260c4587c14356c982a2bf
LakshmanReddyBasi <22pa1a0514@vishnu.edu.in>
2024-12-25 21:53:37 +0530

    Create SingleElement.java

---
+++ SingleElement.java
@@ -0,0 +1,21 @@
+
+
+import java.util.*;
+public class Solution
+{
+    public static int singleNonDuplicate(ArrayList<Integer> arr)
+    {
+        //    Write your code here.
+        HashMap <Integer,Integer> map = new HashMap<>();
+        for (int ele:arr){
+            map.put(ele,map.getOrDefault(ele,0)+1);
+        }
+        for(int ele:map.keySet()){
+            if(map.get(ele) == 1){
+                return ele;
+            }
+        }
+        return -1;
+    }
+       
+}
